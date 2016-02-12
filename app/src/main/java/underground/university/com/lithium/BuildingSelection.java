287ec9d26928b2a27830d15ff65afced7fe947e0
unknown <hugong@D361-S28.iut-acy.local>
2016-02-12 09:07:10 +0100

    Initial commit

---
+++ app/src/main/java/underground/university/com/lithium/BuildingSelection.java
@@ -0,0 +1,13 @@
+package underground.university.com.lithium;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuildingSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_building_selection);
+    }
+}
