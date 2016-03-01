546a74c4fd601d598e9a83bacbca5e74daa96e16
Rippax <dj.jake7@gmail.com>
2016-03-01 17:06:29 +0100

    CalendarParser update

--- app/src/main/java/underground/university/com/lithium/CalendarEvent.java
+++ app/src/main/java/underground/university/com/lithium/CalendarEvent.java
@@ -1,7 +1,60 @@
 package underground.university.com.lithium;
 
+import java.util.*;
+
 /**
  * Created by vivancoe on 01/03/2016.
  */
 public class CalendarEvent {
+
+    public Calendar getStart() {
+        return start;
+    }
+
+    public void setStart(Calendar start) {
+        this.start = start;
+    }
+
+    public Calendar getEnd() {
+        return end;
+    }
+
+    public void setEnd(Calendar end) {
+        this.end = end;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private Calendar start;
+    private Calendar end;
+    private String caption;
+    private String location;
+    private String description;
+
+    public CalendarEvent()
+    {
+
+    }
 }
--- app/src/main/java/underground/university/com/lithium/CalendarParser.java
+++ app/src/main/java/underground/university/com/lithium/CalendarParser.java
@@ -3,6 +3,7 @@ package underground.university.com.lithium;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.concurrent.*;
 
 /**
  * Created by vivancoe on 01/03/2016.
@@ -13,22 +14,28 @@ public class CalendarParser {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
+    private String url = "";
+
+    public String get_iCalContent() {
+        return iCalContent;
     }
 
-    private String url = "";
     private String iCalContent = "";
+
+    public List<CalendarEvent> getEvents() {
+        return events;
+    }
+
     private List<CalendarEvent> events = new ArrayList<CalendarEvent>();
 
     public CalendarParser(String url)
     {
-        setUrl(url);
+        this.url = url;
     }
 
     public void Download_iCal() throws Exception
     {
-        URL url = new URL(getUrl());
+        URL url = new URL(this.url);
         URLConnection connection = url.openConnection();
         BufferedReader streamReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
@@ -41,8 +48,59 @@ public class CalendarParser {
         streamReader.close();
     }
 
+    private Calendar ParseDate(String input)
+    {
+        Calendar date = Calendar.getInstance();
+        int year = Integer.parseInt(input.substring(0, 4));
+        int month = Integer.parseInt(input.substring(4, 6));
+        int day = Integer.parseInt(input.substring(6, 8));
+        int hours = Integer.parseInt(input.substring(9, 11));
+        int minutes = Integer.parseInt(input.substring(11, 13));
+        int seconds = Integer.parseInt(input.substring(13, 15));
+        date.set(year, month, day, hours, minutes, seconds);
+        return date;
+    }
+
+    private String GetValue(String input, String property)
+    {
+        return input.replace(property, "");
+    }
+
     public void Parse()
     {
+        boolean readingEvent = false;
+        CalendarEvent event = new CalendarEvent();
 
+        for(String line : get_iCalContent().split("\r\n"))
+        {
+            line = line.toUpperCase();
+
+            if(readingEvent)
+            {
+                if(line.startsWith("DTSTART:"))
+                    event.setStart(ParseDate(GetValue(line, "DTSTART:")));
+                else if(line.startsWith("DTEND:"))
+                    event.setStart(ParseDate(GetValue(line, "DTEND:")));
+                else if(line.startsWith("SUMMARY:"))
+                    event.setCaption(GetValue(line, "SUMMARY:"));
+                else if(line.startsWith("LOCATION:"))
+                    event.setLocation(GetValue(line, "LOCATION:"));
+                else if(line.startsWith("DESCRIPTION:"))
+                    event.setDescription(GetValue(line, "DESCRIPTION:"));
+                else if(line.startsWith("END:VEVENT"))
+                {
+                    readingEvent = false;
+                    events.add(event);
+                }
+            }
+            else
+            {
+                if(line == "BEGIN:VEVENT")
+                {
+                    readingEvent = true;
+                    event = new CalendarEvent();
+                }
+            }
+        }
     }
 }
